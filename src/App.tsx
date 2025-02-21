0ad059045f018e49fd77adae0e404960522956fd
kimseungjin <ktk5594@naver.com>
2025-02-21 10:54:43 +0900

    remove: app.tsx 테스트 코드 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,7 @@
 import "./styles/App.css";
-import Header from "../src/layouts/Header";
 
 function App() {
-  return (
-    <>
-      <Header
-        type="main"
-        title="홈"
-        hasBackButton={true}
-        hasIcons={true}
-        iconCount={1}
-      />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
