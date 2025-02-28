86318a7b79dd72214e6e62273afcec16494d69f6
kimseungjin <ktk5594@naver.com>
2025-02-28 14:30:07 +0900

    feat (ui) : 글쓰기 UI 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,36 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
-import { LoginPage } from './pages/Login/LoginPage'
-import { useAuthStore } from './stores/authStore'
-import FindAccount from './pages/FindAccount/FindAccount'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { LoginPage } from "./pages/Login/LoginPage";
+import { useAuthStore } from "./stores/authStore";
+import FindAccount from "./pages/FindAccount/FindAccount";
+import { BoardWrite } from "./pages/Board/BoardWrite";
+import "./styles/App.css";
 
 function App() {
-  const { isLoggedIn } = useAuthStore()
+  const { isLoggedIn } = useAuthStore();
 
   return (
     <Router>
       <Routes>
-        <Route 
-          path="/login" 
-          element={
-            isLoggedIn ? <Navigate to="/" replace /> : <LoginPage />
-          } 
+        <Route
+          path="/login"
+          element={isLoggedIn ? <Navigate to="/" replace /> : <LoginPage />}
         />
-        <Route 
-          path="/" 
+        <Route
+          path="/"
           element={
             isLoggedIn ? <div>홈페이지</div> : <Navigate to="/login" replace />
-          } 
+          }
         />
         <Route path="/find-account" element={<FindAccount />} />
+        <Route path="/write" element={<BoardWrite />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
