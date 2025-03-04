2a3dfbcf8c4db15db05ab3d3f2a40a1bc59f46bd
kimseungjin <ktk5594@naver.com>
2025-03-04 11:14:29 +0900

    feat (cors) : cors 에러 서버 프록시 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://13.124.36.165:8080",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
