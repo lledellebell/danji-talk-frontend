134f76ce176ba00e201bc749ffd6821756d93323
kimseungjin <ktk5594@naver.com>
2025-03-06 22:44:51 +0900

    fix (rewrite) :  rewrite api 경로 삭제 기능 제거

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
-  
+
   return {
     plugins: [react()],
     server: {
@@ -13,8 +13,7 @@ export default defineConfig(({ mode }) => {
           target: env.VITE_API_URL,
           changeOrigin: true,
           secure: true,
-          rewrite: (path) => path.replace(/^\/api/, '')
-        }
+        },
       },
     },
   };
