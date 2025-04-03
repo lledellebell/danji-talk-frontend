06e4cf99da97ce033a8c6034e03a40d4c3c5ea54
deep <function.hola.b@gmail.com>
2025-04-04 05:48:06 +0900

    feat(MenuGrid): 메뉴 아이콘을 SVG 이미지로 변경 및 스타일 수정 - 아이콘 크기 및 색상 조정

--- src/components/MenuGrid/MenuGrid.tsx
+++ src/components/MenuGrid/MenuGrid.tsx
@@ -1,15 +1,23 @@
 import { Link } from 'react-router-dom';
 import styles from './MenuGrid.module.scss';
+import ComplexInfoIcon from '../../assets/menugrid/Chart.svg';
+import CommunityIcon from '../../assets/menugrid/Gamepad.svg';
+import NoticesIcon from '../../assets/menugrid/Danger.svg';
+import FacilitiesIcon from '../../assets/menugrid/Graph.svg';
+import MypageIcon from '../../assets/menugrid/User.svg';
+import FavoritesIcon from '../../assets/menugrid/Star.svg';
+import ChatIcon from '../../assets/menugrid/Message.svg';
+import VisitorCarIcon from '../../assets/menugrid/Receipt.svg';
 
 const menuItems = [
-  { icon: '..', label: '단지정보', path: '/complex-info' },
-  { icon: '..', label: '커뮤니티', path: '/community' },
-  { icon: '..', label: '공지사항', path: '/notices' },
-  { icon: '..', label: '시설정보', path: '/facilities' },
-  { icon: '..', label: '마이페이지', path: '/mypage' },
-  { icon: '..', label: '즐겨찾기', path: '/favorites' },
-  { icon: '..', label: '채팅', path: '/chat' },
-  { icon: '..', label: '방문차량', path: '/visitor-car' },
+  { icon: <img className={styles.icon} src={ComplexInfoIcon} alt="단지정보" />, label: '단지정보', path: '/complex-info' },
+  { icon: <img className={styles.icon} src={CommunityIcon} alt="커뮤니티" />, label: '커뮤니티', path: '/community' },
+  { icon: <img className={styles.icon} src={NoticesIcon} alt="공지사항" />, label: '공지사항', path: '/notices' },
+  { icon: <img className={styles.icon} src={FacilitiesIcon} alt="시설정보" />, label: '시설정보', path: '/facilities' },
+  { icon: <img className={styles.icon} src={MypageIcon} alt="마이페이지" />, label: '마이페이지', path: '/mypage' },
+  { icon: <img className={styles.icon} src={FavoritesIcon} alt="즐겨찾기" />, label: '즐겨찾기', path: '/favorites' },
+  { icon: <img className={styles.icon} src={ChatIcon} alt="채팅" />, label: '채팅', path: '/chat' },
+  { icon: <img className={styles.icon} src={VisitorCarIcon} alt="방문차량" />, label: '방문차량', path: '/visitor-car' },
 ];
 
 const MenuGrid = () => {
@@ -22,7 +30,7 @@ const MenuGrid = () => {
           className={styles.menuItem}
           aria-label={item.label}
         >
-          <span className={styles.icon}>{item.icon}</span>
+          {item.icon}
           <span className={styles.label}>{item.label}</span>
         </Link>
       ))}
