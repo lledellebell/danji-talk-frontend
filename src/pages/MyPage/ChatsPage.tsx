0e21d7f8fde7136b281f0f1cbb54adeddce87580
deep <function.hola.b@gmail.com>
2025-04-11 06:38:55 +0900

    feat(pages): 기존 페이지 삭제 및 새로운 페이지 컴포넌트 추가, '채팅방 목록', '작성한 글', '스크랩한 글', '방문차량 목록'에 대한 건설 중 알림 기능 통합

---
+++ src/pages/MyPage/ChatsPage.tsx
@@ -0,0 +1,20 @@
+import Header from '../../layouts/Header';
+import useUnderConstructionAlert from '../../hooks/useUnderConstructionAlert';
+
+const ChatsPage = () => {
+  useUnderConstructionAlert('채팅방 목록');
+
+  return (
+    <>
+      <Header 
+        title="채팅방 목록" 
+        type="sub" 
+        hasBackButton={true}
+      />
+      <div style={{ padding: '20px', textAlign: 'center' }}>
+      </div>
+    </>
+  );
+};
+
+export default ChatsPage; 
