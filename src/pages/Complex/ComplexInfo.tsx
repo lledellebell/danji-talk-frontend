4bbf31a567e16c3841094087e6ca0962c73ac82c
deep <function.hola.b@gmail.com>
2025-04-11 09:45:07 +0900

    feat(complex): 단지 등록 및 정보 페이지 추가, 주소 검색 기능 통합, 스타일 및 구조 개선

---
+++ src/pages/Complex/ComplexInfo.tsx
@@ -0,0 +1,13 @@
+import Header from '../../layouts/Header';
+
+const ComplexInfo = () => {
+  return (
+    <>
+      <Header title="단지정보" type="sub" hasBackButton={true} />
+      <div style={{padding: '20px'}}>
+      </div>
+    </>
+  );
+};
+
+export default ComplexInfo; 
