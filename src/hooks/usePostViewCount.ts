7b7d7faa961e38948780dc47efe1e8a2a87b5326
kimseungjin <ktk5594@naver.com>
2025-04-16 22:26:19 +0900

    feat (viewCount): 게시글 조회시 조회수 API 호출 기능 추가

---
+++ src/hooks/usePostViewCount.ts
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import { useMutation } from '@tanstack/react-query';
+
+export const usePostViewCount = () => {
+  const viewCountMutaion = useMutation({
+    mutationFn: async (feedId: number) => {
+      const res = await axios.post(`/api/community/feeds/${feedId}/view`);
+      return res.data;
+    },
+    onSuccess: () => {},
+    onError: (error) => {},
+  });
+
+  return { postViewCount: viewCountMutaion.mutate };
+};
