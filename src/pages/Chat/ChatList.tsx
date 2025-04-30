3128acc93dfdb67918815fd65e15edf78f1436ee
kimseungjin <ktk5594@naver.com>
2025-04-30 16:31:07 +0900

    feat (chat): 보낸 , 받은 요청 api 연동

--- src/pages/Chat/ChatList.tsx
+++ src/pages/Chat/ChatList.tsx
@@ -1,282 +1,134 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import TabWrapper from '../../components/common/Tab/TabWrapper';
 import TabPanel from '../../components/common/Tab/TabPanel';
 import Tab from '../../components/common/Tab/Tab';
 import profileIcon from '../../assets/board/profile.svg';
 import moreVerticalIcon from '../../assets/board/moreVertical.svg';
+import { formatDate } from '../../utils/formatDate';
 import {
   ChatRoom,
   useDirectChats,
   useGroupChats,
+  useRequestChats,
+  useResponseChats,
 } from '../../services/chatService';
 import styles from './ChatList.module.scss';
+import React from 'react';
 
-const OneToOneChat = () => {
+const OneToOneChat = ({ directChats }: { directChats: ChatRoom[] }) => {
   return (
     <>
-      <div className={styles['one-to-one-chat']}>
-        <div className={styles['one-to-one-chat-container']}>
-          <div className={styles['one-to-one-chat-info']}>
-            <img
-              className={styles['one-to-one-chat-profile-img']}
-              src={profileIcon}
-              alt="프로필"
-            />
-            <div className={styles['one-to-one-chat-author']}>
-              <div>
-                <span className={styles['one-to-one-chat-name']}>한예빈</span>
-                <span className={styles['one-to-one-chat-time']}>3분 전</span>
+      {directChats.map((chat, index) => (
+        <React.Fragment key={index}>
+          <div className={styles['one-to-one-chat']}>
+            <div className={styles['one-to-one-chat-container']}>
+              <div className={styles['one-to-one-chat-info']}>
+                <img
+                  className={styles['one-to-one-chat-profile-img']}
+                  src={profileIcon}
+                  alt="프로필"
+                />
+                <div className={styles['one-to-one-chat-author']}>
+                  <div>
+                    <span className={styles['one-to-one-chat-name']}>
+                      {chat.name}
+                    </span>
+                    <span className={styles['one-to-one-chat-time']}>
+                      {chat.time}
+                    </span>
+                  </div>
+                  <p className={styles['one-to-one-chat-content']}>
+                    {chat.lastMessage}
+                  </p>
+                </div>
               </div>
-              <p className={styles['one-to-one-chat-content']}>
-                저도 그런 이유로 입주를 고민하고 있었어요. 혹시 입주 예정일은
-                언제쯤이세요?
-              </p>
+              <div className={styles['one-to-one-chat-buttons']}>
+                <img
+                  src={moreVerticalIcon}
+                  alt="더보기"
+                  className={styles['one-to-one-chat-more-icon']}
+                />
+              </div>
             </div>
           </div>
-
-          <div className={styles['one-to-one-chat-buttons']}>
-            <img
-              src={moreVerticalIcon}
-              alt="더보기"
-              className={styles['one-to-one-chat-more-icon']}
-            />
-          </div>
-        </div>
-      </div>
-      <div className={styles['one-to-one-chat-bottom-divider']}></div>
-      <div className={styles['one-to-one-chat']}>
-        <div className={styles['one-to-one-chat-container']}>
-          <div className={styles['one-to-one-chat-info']}>
-            <img
-              className={styles['one-to-one-chat-profile-img']}
-              src={profileIcon}
-              alt="프로필"
-            />
-            <div className={styles['one-to-one-chat-author']}>
-              <div>
-                <span className={styles['one-to-one-chat-name']}>김민수</span>
-                <span className={styles['one-to-one-chat-time']}>10분 전</span>
-              </div>
-              <p className={styles['one-to-one-chat-content']}>그런가요?ㅎㅎ</p>
-            </div>
-          </div>
-
-          <div className={styles['one-to-one-chat-buttons']}>
-            <img
-              src={moreVerticalIcon}
-              alt="더보기"
-              className={styles['one-to-one-chat-more-icon']}
-            />
-          </div>
-        </div>
-      </div>
-      <div className={styles['one-to-one-chat-bottom-divider']}></div>
-      <div className={styles['one-to-one-chat']}>
-        <div className={styles['one-to-one-chat-container']}>
-          <div className={styles['one-to-one-chat-info']}>
-            <img
-              className={styles['one-to-one-chat-profile-img']}
-              src={profileIcon}
-              alt="프로필"
-            />
-            <div className={styles['one-to-one-chat-author']}>
-              <div>
-                <span className={styles['one-to-one-chat-name']}>오세현</span>
-                <span className={styles['one-to-one-chat-time']}>25.02.10</span>
-              </div>
-              <p className={styles['one-to-one-chat-content']}>안녕하세요</p>
-            </div>
-          </div>
-
-          <div className={styles['one-to-one-chat-buttons']}>
-            <img
-              src={moreVerticalIcon}
-              alt="더보기"
-              className={styles['one-to-one-chat-more-icon']}
-            />
-          </div>
-        </div>
-      </div>
-      <div className={styles['one-to-one-chat-bottom-divider']}></div>
-      <div className={styles['one-to-one-chat']}>
-        <div className={styles['one-to-one-chat-container']}>
-          <div className={styles['one-to-one-chat-info']}>
-            <img
-              className={styles['one-to-one-chat-profile-img']}
-              src={profileIcon}
-              alt="프로필"
-            />
-            <div className={styles['one-to-one-chat-author']}>
-              <div>
-                <span className={styles['one-to-one-chat-name']}>이진우</span>
-                <span className={styles['one-to-one-chat-time']}>25.02.08</span>
-              </div>
-              <p className={styles['one-to-one-chat-content']}>
-                아파트 관련해서 정보 좀 얻고자 연락드렸어요. 혹시 아파트 도서관
-                사용을 위해 정액권을 끊어야 하나요?
-              </p>
-            </div>
-          </div>
-
-          <div className={styles['one-to-one-chat-buttons']}>
-            <img
-              src={moreVerticalIcon}
-              alt="더보기"
-              className={styles['one-to-one-chat-more-icon']}
-            />
-          </div>
-        </div>
-      </div>
-      <div className={styles['one-to-one-chat-bottom-divider']}></div>
-      <div className={styles['one-to-one-chat']}>
-        <div className={styles['one-to-one-chat-container']}>
-          <div className={styles['one-to-one-chat-info']}>
-            <img
-              className={styles['one-to-one-chat-profile-img']}
-              src={profileIcon}
-              alt="프로필"
-            />
-            <div className={styles['one-to-one-chat-author']}>
-              <div>
-                <span className={styles['one-to-one-chat-name']}>김재유</span>
-                <span className={styles['one-to-one-chat-time']}>25.01.28</span>
-              </div>
-              <p className={styles['one-to-one-chat-content']}>
-                정말요? 너무 유익하네요
-              </p>
-            </div>
-          </div>
-
-          <div className={styles['one-to-one-chat-buttons']}>
-            <img
-              src={moreVerticalIcon}
-              alt="더보기"
-              className={styles['one-to-one-chat-more-icon']}
-            />
-          </div>
-        </div>
-      </div>
-      <div className={styles['one-to-one-chat-bottom-divider']}></div>
+          <div className={styles['one-to-one-chat-bottom-divider']}></div>
+        </React.Fragment>
+      ))}
     </>
   );
 };
 
-const RequestChat = () => {
+const ResponseChat = ({ responseChats }: { responseChats: ChatRoom[] }) => {
   return (
     <>
-      <div className={styles['chat-request']}>
-        <div className={styles['chat-request-container']}>
-          <div className={styles['chat-request-info']}>
-            <img
-              className={styles['chat-request-profile-img']}
-              src={profileIcon}
-              alt="프로필"
-            />
-            <div className={styles['chat-request-author']}>
-              <span className={styles['chat-request-name']}>
-                <span className={styles['chat-request-name']}>한예빈</span>
-                <span className={styles['chat-request-time']}>3분 전</span>
-              </span>
-              <span className={styles['chat-request-content']}>
-                아파트 관련해서 정보 좀 얻고자 연락드렸어요. 혹시 아파트 도서관
-                사용을 위해 정액권을 끊어야 하나요?
-              </span>
+      {responseChats.map((chat, idx) => (
+        <div className={styles['chat-response']}>
+          <div className={styles['chat-response-container']}>
+            <div className={styles['chat-response-info']}>
+              <img
+                className={styles['chat-response-profile-img']}
+                src={profileIcon}
+                alt="프로필"
+              />
+              <div className={styles['chat-response-author']}>
+                <span className={styles['chat-response-name']}>
+                  <span className={styles['chat-response-name']}>한예빈</span>
+                  <span className={styles['chat-response-time']}>3분 전</span>
+                </span>
+                <span className={styles['chat-response-content']}>
+                  아파트 관련해서 정보 좀 얻고자 연락드렸어요. 혹시 아파트
+                  도서관 사용을 위해 정액권을 끊어야 하나요?
+                </span>
+              </div>
+            </div>
+            <div className={styles['chat-response-buttons']}>
+              <span className={styles['chat-response-reject']}>거절</span>
+              <div className={styles['chat-response-divider']}></div>
+              <span className={styles['chat-response-accept']}>수락</span>
             </div>
           </div>
-          <div className={styles['chat-request-buttons']}>
-            <span className={styles['chat-request-reject']}>거절</span>
-            <div className={styles['chat-request-divider']}></div>
-            <span className={styles['chat-request-accept']}>수락</span>
-          </div>
+          <div className={styles['chat-response-bottom-divider']}></div>
         </div>
-        <div className={styles['chat-request-bottom-divider']}></div>
-      </div>
-      <div className={styles['chat-request']}>
-        <div className={styles['chat-request-container']}>
-          <div className={styles['chat-request-info']}>
-            <img
-              className={styles['chat-request-profile-img']}
-              src={profileIcon}
-              alt="프로필"
-            />
-            <div className={styles['chat-request-author']}>
-              <span className={styles['chat-request-name']}>
-                <span className={styles['chat-request-name']}>이진아</span>
-                <span className={styles['chat-request-time']}>3분 전</span>
-              </span>
-              <span className={styles['chat-request-content']}>
-                안녕하세요!
-              </span>
-            </div>
-          </div>
-          <div className={styles['chat-request-buttons']}>
-            <span className={styles['chat-request-reject']}>거절</span>
-            <div className={styles['chat-request-divider']}></div>
-            <span className={styles['chat-request-accept']}>수락</span>
-          </div>
-        </div>
-        <div className={styles['chat-request-bottom-divider']}></div>
-      </div>
+      ))}
     </>
   );
 };
 
-const ResponseChat = () => {
+const RequestChat = ({ requestChats }: { requestChats: ChatRoom[] }) => {
   return (
     <>
-      <div className={styles['chat-response']}>
-        <div className={styles['chat-response-container']}>
-          <div className={styles['chat-response-info']}>
-            <img
-              className={styles['chat-response-profile-img']}
-              src={profileIcon}
-              alt="프로필"
-            />
-            <div className={styles['chat-response-author']}>
-              <span className={styles['chat-response-name']}>
-                <span className={styles['chat-response-name']}>한예빈</span>
-                <span className={styles['chat-response-time']}>3분 전</span>
-              </span>
-              <span className={styles['chat-response-content']}>
-                아파트 관련해서 정보 좀 얻고자 연락드렸어요. 혹시 아파트 도서관
-                사용을 위해 정액권을 끊어야 하나요?
-              </span>
+      {requestChats.map((chat, idx) => (
+        <div key={idx} className={styles['chat-request']}>
+          <div className={styles['chat-request-container']}>
+            <div className={styles['chat-request-info']}>
+              <img
+                className={styles['chat-request-profile-img']}
+                src={profileIcon}
+                alt="프로필"
+              />
+              <div className={styles['chat-request-author']}>
+                <span className={styles['chat-request-name']}>
+                  <span className={styles['chat-request-name']}>
+                    {chat.memberInformation.nickname}
+                  </span>
+                  <span className={styles['chat-request-time']}>
+                    {formatDate(chat.createdAt)}
+                  </span>
+                </span>
+                <span className={styles['chat-request-content']}>
+                  {chat.message}
+                </span>
+              </div>
+            </div>
+            <div className={styles['chat-request-buttons']}>
+              <span className={styles['chat-request-status']}>대기중</span>
+              <div className={styles['chat-request-divider']}></div>
+              <span className={styles['chat-request-cancel']}>취소</span>
             </div>
           </div>
-          <div className={styles['chat-response-buttons']}>
-            <span className={styles['chat-response-status']}>대기중</span>
-            <div className={styles['chat-response-divider']}></div>
-            <span className={styles['chat-response-cancel']}>취소</span>
-          </div>
+          <div className={styles['chat-request-bottom-divider']}></div>
         </div>
-        <div className={styles['chat-response-bottom-divider']}></div>
-      </div>
-      <div className={styles['chat-response']}>
-        <div className={styles['chat-response-container']}>
-          <div className={styles['chat-response-info']}>
-            <img
-              className={styles['chat-response-profile-img']}
-              src={profileIcon}
-              alt="프로필"
-            />
-            <div className={styles['chat-response-author']}>
-              <span className={styles['chat-response-name']}>
-                <span className={styles['chat-response-name']}>이진아</span>
-                <span className={styles['chat-response-time']}>3분 전</span>
-              </span>
-              <span className={styles['chat-response-content']}>
-                안녕하세요!
-              </span>
-            </div>
-          </div>
-          <div className={styles['chat-response-buttons']}>
-            <span className={styles['chat-response-status']}>대기중</span>
-            <div className={styles['chat-response-divider']}></div>
-            <span className={styles['chat-response-cancel']}>취소</span>
-          </div>
-        </div>
-        <div className={styles['chat-response-bottom-divider']}></div>
-      </div>
+      ))}
     </>
   );
 };
@@ -296,7 +148,23 @@ export const ChatList = () => {
     error: groupChatsError,
   } = useGroupChats();
 
+  const {
+    data: requestChats = [],
+    isLoading: isRequestChatsLoading,
+    error: requestChatsError,
+  } = useRequestChats();
+
+  const {
+    data: responseChats = [],
+    isLoading: isResponseChatsLoading,
+    error: responseChatsError,
+  } = useResponseChats();
+
   const handleError = (error: Error) => {
+    if (!error) {
+      return '에러';
+    }
+
     if (error.message.includes('인증이 필요합니다')) {
       // 로그인 페이지로 리다이렉트하거나 로그인 모달을 표시
       return '로그인이 필요한 서비스입니다.';
@@ -381,7 +249,6 @@ export const ChatList = () => {
           id="tabpanel-0"
           ariaLabelledby="tab-0"
         >
-          <OneToOneChat />
           <div className={styles['chat-list__container']}>
             {isDirectChatsLoading ? (
               <div className={styles['chat-list__loading']}>로딩 중...</div>
@@ -389,16 +256,15 @@ export const ChatList = () => {
               <div className={styles['chat-list__error']}>
                 {handleError(directChatsError as Error)}
               </div>
-            ) : directChats.length === 0 ? (
+            ) : directChats.data.length === 0 ? (
               <div className={styles['chat-list__empty']}>
                 1:1 채팅이 없습니다
               </div>
             ) : (
-              directChats.map(renderChatItem)
+              <OneToOneChat directChats={directChats} />
             )}
           </div>
         </TabPanel>
-
         <TabPanel
           isActive={activeTab === 1}
           role="tabpanel"
@@ -412,23 +278,37 @@ export const ChatList = () => {
               <div className={styles['chat-list__error']}>
                 {handleError(groupChatsError as Error)}
               </div>
-            ) : groupChats.length === 0 ? (
+            ) : groupChats.data.length === 0 ? (
               <div className={styles['chat-list__empty']}>
                 단체 채팅이 없습니다
               </div>
             ) : (
-              groupChats.map(renderChatItem)
+              // groupChats.map(renderChatItem)
+              <></>
             )}
           </div>
         </TabPanel>
-
         <TabPanel
           isActive={activeTab === 2}
           role="tabpanel"
           id="tabpanel-2"
           ariaLabelledby="tab-2"
         >
-          <RequestChat />
+          <div className={styles['chat-list__container']}>
+            {isResponseChatsLoading ? (
+              <div className={styles['chat-list__loading']}>로딩 중...</div>
+            ) : responseChats ? (
+              <div className={styles['chat-list__error']}>
+                {handleError(responseChatsError as Error)}
+              </div>
+            ) : responseChats.data.length === 0 ? (
+              <div className={styles['chat-list__empty']}>
+                받은 채팅이 없습니다
+              </div>
+            ) : (
+              <ResponseChat responseChats={responseChats.data} />
+            )}
+          </div>
         </TabPanel>
         <TabPanel
           isActive={activeTab === 3}
@@ -436,7 +316,21 @@ export const ChatList = () => {
           id="tabpanel-3"
           ariaLabelledby="tab-3"
         >
-          <ResponseChat />
+          <div className={styles['chat-list__container']}>
+            {isRequestChatsLoading ? (
+              <div className={styles['chat-list__loading']}>로딩 중...</div>
+            ) : requestChatsError ? (
+              <div className={styles['chat-list__error']}>
+                {handleError(requestChatsError as Error)}
+              </div>
+            ) : requestChats.data.length === 0 ? (
+              <div className={styles['chat-list__empty']}>
+                보낸 채팅이 없습니다
+              </div>
+            ) : (
+              <RequestChat requestChats={requestChats.data} />
+            )}
+          </div>
         </TabPanel>
       </TabWrapper>
     </div>
--- src/services/chatService.ts
+++ src/services/chatService.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useQuery } from '@tanstack/react-query';
 
 const api = axios.create({
-  baseURL: 'https://danjitalk.duckdns.org/api',
+  baseURL: '/api',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
@@ -39,20 +39,30 @@ api.interceptors.request.use(
 export interface ChatRoom {
   id: string;
   name: string; // 채팅방 이름
+  time: string; // 채팅방 시간
+  message: string; // 요청 메세지
+  createdAt: string; // 요청 보낸 시간
   lastMessage?: string; // 마지막 메시지
   updatedAt: string; // 마지막 업데이트 시간
   unreadCount: number; // 읽지 않은 메시지 수
-  participants: {
+  participants: { id: string; name: string; profileImage?: string }[];
+  memberInformation: {
     // 참여자 정보
     id: string;
-    name: string;
+    nickname: string;
     profileImage?: string;
-  }[];
+  };
 }
 
 export const chatKeys = {
   all: ['chats'] as const,
+
+  // 1:1 채팅
   direct: () => [...chatKeys.all, 'direct'] as const,
+  directRequest: () => [...chatKeys.all, 'request'] as const,
+  directResponse: () => [...chatKeys.all, 'response'] as const,
+
+  // 그룹 채팅
   group: () => [...chatKeys.all, 'group'] as const,
   room: (id: string) => [...chatKeys.all, 'room', id] as const,
 };
@@ -60,20 +70,34 @@ export const chatKeys = {
 export const chatService = {
   // 1:1 채팅 목록 조회
   getDirectChats: async () => {
-    await api.get('/chat/direct');
+    const response = await api.get('/chat/direct');
+    return response.data;
   },
 
   // 단체 채팅 목록 조회
   getGroupChats: async () => {
-    await api.get('/chat/group');
+    const response = await api.get('/chat/group');
+    return response.data;
+  },
+
+  // 보낸 요청
+  getRequestChats: async () => {
+    const response = await api.get('/chat/request/sent');
+    return response.data;
+  },
+
+  // 받은 요청
+  getResponseChats: async () => {
+    const response = await api.get('/chat/request/received');
+    return response.data;
   },
 
   // 채팅방 상세 조회
   getChatRoomDetail: async (id: string) => {
-    await api.get(`/chat/room/${id}`);
+    const response = await api.get(`/chat/room/${id}`);
+    return response.data;
   },
 };
-
 export const useDirectChats = () => {
   return useQuery({
     queryKey: chatKeys.direct(),
@@ -86,6 +110,22 @@ export const useGroupChats = () => {
   return useQuery({
     queryKey: chatKeys.group(),
     queryFn: chatService.getGroupChats,
+    retry: false,
+  });
+};
+
+export const useRequestChats = () => {
+  return useQuery({
+    queryKey: chatKeys.directRequest(),
+    queryFn: chatService.getRequestChats,
+    retry: false,
+  });
+};
+
+export const useResponseChats = () => {
+  return useQuery({
+    queryKey: chatKeys.directResponse(),
+    queryFn: chatService.getResponseChats,
     retry: false,
   });
 };
